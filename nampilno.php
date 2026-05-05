b9174b8b6752ce7cf9e9eb172bc0ef0b97ac92e4
Kertcode <zainalzainalll276@gmail.com>
2026-05-05 14:47:57 +0700

    Refactor nampilno.php for improved readability

--- nampilno.php
+++ nampilno.php
@@ -1,12 +1,11 @@
 <?php
-
 // jeneng file JSON data Jawa Timur
 $jeneng_file = "data_jatim.json";
 
 // maca isi file JSON
 $data_json = file_get_contents($jeneng_file);
 
-// yen file ora iso diwaca
+// jika file gak isok diwaca
 if($data_json == false){
     echo "File ora iso diwaca\n";
     exit;
@@ -24,19 +23,16 @@ echo "=== DATA DBHCHT JAWA TIMUR 2025 ===\n\n";
 // nomer awal
 $nomer = 1;
 
-// looping nampilake isi data
+// looping nampilno isi data
 foreach($daftar_data as $baris){
-
     echo $nomer . ". ";
     echo "Provinsi : " . $baris["nama_provinsi"] . "\n";
     echo "Kategori : " . $baris["kategori"] . "\n";
     echo "Jumlah   : " . $baris["jumlah"] . " " . $baris["satuan"] . "\n";
     echo "Tahun    : " . $baris["tahun"] . "\n\n";
-
     $nomer++;
 }
 
 // pesan akhir
 echo "Rampung nampilno data Jawa Timur.\n";
-
 ?>
